68d13c3061057f7ca6284137bd52ce9948096511
Martin Wimpress <martin.wimpress@flightdataservices.com>
2013-04-09 13:49:58 +0100

    Reverted.

--- flightdataparametertree/server.py
+++ flightdataparametertree/server.py
@@ -99,9 +99,9 @@ AJAX_DIR = os.path.join(APPDATA_DIR, 'ajax')
 if not os.path.isdir(AJAX_DIR):
     print "Making AJAX directory: %s" % AJAX_DIR
     os.makedirs(AJAX_DIR)
-    
+
 socket.setdefaulttimeout(120)
-    
+
 ################################################################################
 # Helpers
 
@@ -147,9 +147,7 @@ def lookup_path(relative_path):
     '''
     Convert a relative path to the asset path. Accounts for being frozen.
     '''
-    print relative_path
     file_path = relative_path.lstrip('/').replace('/', os.sep)
-    print file_path
     if getattr(sys, 'frozen', False):
         # The OLD pyInstaller way
         # http://www.pyinstaller.org/export/v1.5.1/project/doc/Manual.html?format=raw#accessing-data-files
@@ -159,8 +157,7 @@ def lookup_path(relative_path):
         #else:
         #    # --onedir distribution
         #    return os.path.join(os.path.dirname(sys.executable), file_path)
-        print os.path.join(os.path.dirname('.'), file_path)
-        return os.path.join(os.path.dirname('.'), file_path)
+        return os.path.join(os.path.dirname(sys.executable), file_path)
     else:
         return file_path
 
